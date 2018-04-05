37a66e278eb8d099f3d5c2fa5a714359c8178a2c
Umar Muneer <umarmuneer@Educative7s-MacBook-Pro.local>
2018-04-05 12:34:49 +0500

    added a / callback

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ app.get('/auth0/callback', (req, res) => {
     const state = req.query.state;
     res.json(`state:${state}`);
 });
+app.get('/', (req, res) => {
+    res.json('i am here!');
+})
 app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`));
